e13ec56d64ce8277a2c8127d9cd1c257d42cc17b
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-12-09 13:46:42 +0530

    do while loop

---
+++ DOWHILE.C
@@ -0,0 +1,21 @@
+//do while loop
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ int i=2;
+ clrscr();
+
+ do
+ {
+  printf("%d",i);
+  i=i+1;
+  printf(" ");
+ }
+
+ while(i<=10);
+
+getch();
+}
